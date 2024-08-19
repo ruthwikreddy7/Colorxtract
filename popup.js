5a79edbb67f0daa05e0a2c75c6f7d630f0e8513b
Ruthwik Reddy Lakkireddy <81750506+ruthwikreddy7@users.noreply.github.com>
2024-08-19 12:32:08 +0530

    ..

--- popup.js
+++ popup.js
@@ -1,79 +1,40 @@
+const btn = document.querySelector('.changeColorBtn');
+const colorGrid = document.querySelector('.colorGrid');
+const colorValue = document.querySelector('.colorValue');
 
-window.addEventListener('DOMContentLoaded', () => {
-  const mainCont = document.getElementById("mainCont");
-  const buttonCont = document.getElementById("picker_btn_cont");
-  const resultList = document.getElementById("result");
+btn.addEventListener('click', async () => {
+    chrome.storage.sync.get('color', ({ color }) => {
+        console.log('color: ', color);
+    });
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  const GiveMetheChild = (color, msg) => {
-      const errorLabel = document.createElement("p")
-      errorLabel.setAttribute("class", "errorLabel")
-      errorLabel.style.backgroundColor = color
-      errorLabel.innerText = msg
+    chrome.scripting.executeScript(
+        {
+            target: { tabId: tab.id },
+            function: pickColor,
+        },
+        async (injectionResults) => {
+            const [data] = injectionResults;
+            if (data.result) {
+                const color = data.result.sRGBHex;
+                colorGrid.style.backgroundColor = color;
+                colorValue.innerText = color;
+                try {
+                    await navigator.clipboard.writeText(color);
+                } catch (err) {
+                    console.error(err);
+                }
+            }
+        }
+    );
+});
 
-      mainCont.appendChild(errorLabel)
-      setTimeout(() => {
-          mainCont.removeChild(errorLabel)
-      }, 1000)
-  }
-
-  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-      const tab = tabs[0]
-
-      if (tab.url === undefined || tab.url.indexOf('chrome') == 0) {
-          buttonCont.innerHTML = '<span Colorxtract</span> can\'t access <i>this tab </i><span> please head over to website where you want to pick your color</span> '
-      }
-      else if (tab.url.indexOf('file') === 0) {
-          buttonCont.innerHTML = '<span Colorxtract</span> can\'t access <i>local pages</i>'
-
-      } else {
-          const button = document.createElement("button")
-          button.setAttribute("id", "picker_btn")
-          button.innerText = "Pick color"
-
-          button.addEventListener("click", () => {
-              if (!window.EyeDropper) {
-                  GiveMetheChild("#ad5049", 'Your browser does not support the EyeDropper API')
-                  return
-              }
-
-              chrome.tabs.sendMessage(
-                  tabs[0].id,
-                  { from: "popup", query: "eye_dropper_clicked" }
-              );
-              window.close()
-          })
-
-          buttonCont.appendChild(button)
-      }
-  });
-
-
-
-
-  chrome.storage.local.get("color_hex_code", (resp) => {
-
-      if (resp.color_hex_code && resp.color_hex_code.length > 0) {
-          resp.color_hex_code.forEach(hexCode => {
-              const liElem = document.createElement("li")
-              liElem.innerText = hexCode
-              liElem.style.backgroundColor = hexCode
-              liElem.addEventListener("click", () => {
-                  navigator.clipboard.writeText(hexCode);
-                  GiveMetheChild("#054BF7", "Hex code is copied to clipboard!")
-              })
-              resultList.appendChild(liElem)
-          })
-
-          const ClearButton = document.createElement("button")
-          ClearButton.innerText = "Clear copied items"
-          ClearButton.setAttribute("id", "ClearButton")
-          ClearButton.addEventListener("click", () => {
-              chrome.storage.local.remove("color_hex_code")
-              window.close()
-          })
-          mainCont.appendChild(ClearButton)
-      }
-
-  })
-
-})
+async function pickColor() {
+    try {
+        // Picker
+        const eyeDropper = new EyeDropper();
+        return await eyeDropper.open();
+    } catch (err) {
+        console.error(err);
+    }
+}
